bfa631a1f4d9f88c9b47fb4c41acb2ba5438bcca
Skyl3r <kpereksta@gmail.com>
2019-03-20 11:27:59 -0400

    Fixed flipped headers

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
 
 
 	<body>
-		<h4>Skyl3r's Page</h4>
-		<h3>Introduction</h3>
+		<h3>Skyl3r's Page</h3>
+		<h4>Introduction</h4>
 		<p>Here you can find a list of personal projects, as well as guides and tutorials</p>
 		
 	</body>
