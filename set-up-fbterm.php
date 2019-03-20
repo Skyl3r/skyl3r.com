8d1409154ad77037622efdcd1477cb831382873a
Skyl3r <kpereksta@gmail.com>
2019-03-20 10:53:49 -0400

    Added tutorials folder and start of tutorial for test.

---
+++ set-up-fbterm.php
@@ -0,0 +1,14 @@
+<html>
+
+	<head>
+		<title>Setting up FBTerm</title>
+	</head>
+
+	<body>
+		<h3>Setting up FBTerm for a Virtual Console</h3>
+		
+		<h4>Introduction</h4>
+		<p>FBTerm (Frame Buffer Terminal) is a terminal interface that can run without Xorg installed. It's not in development anymore but <i>it works</i></p>
+	</body>
+
+</html>
