feae2bc8fabfa49c06840e7d1157d4edb7a277e4
Skyl3r <kpereksta@gmail.com>
2021-05-10 15:00:01 -0400

    Updated index page

--- index.php
+++ index.php
@@ -8,10 +8,41 @@
 
 
 	<body>
-		<h3>Skyl3r's Page</h3>
-		<h4>Introduction</h4>
-		<p>Here you can find a list of personal projects, as well as guides and tutorials</p>
-		
+		<h2>Skyl3r</h2>
+
+		<div id="filterContainer">
+			<button class="btn active" onclick="filterSelection('development')">Development</button>
+			<button class="btn" onclick="filterSelection('overclocking')">Overclocking</button>
+			<button class="btn" onclick="filterSelection('servers')">Servers</button>
+			<button class="btn" onclick="filterSelection('cars')">Cars</button>
+			<button class="btn" onclick="filterSelection('all')">Show all</button>
+		</div>
+
+		<div class="row">
+			<div class="column development">
+				<div class="content">
+					<p>Lorem ipsum</p>
+				</div>
+			</div>
+
+			<div class="column overclocking">
+				<div class="content">
+					<p>Lorem ipsum</p>
+				</div>
+			</div>
+
+			<div class="column servers">
+				<div class="content">
+					<p>Lorem ipsum</p>
+				</div>
+			</div>
+
+			<div class="column cars">
+				<div class="content">
+					<p>Lorem ipsum</p>
+				</div>
+			</div>
+		</div>
 	</body>
 
 
