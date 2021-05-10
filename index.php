c2d8cf9e978c319f6ae95d773e35bed79a0e4b51
Skyl3r <kpereksta@gmail.com>
2021-05-10 15:08:12 -0400

    Added stylesheet

--- index.php
+++ index.php
@@ -4,42 +4,45 @@
 
 	<head>
 		<title>Skyl3r</title>
+		<link rel="stylesheet" href="main.css">
 	</head>
 
 
 	<body>
-		<h2>Skyl3r</h2>
+		<div class="main">
+			<h2>Skyl3r</h2>
 
-		<div id="filterContainer">
-			<button class="btn active" onclick="filterSelection('development')">Development</button>
-			<button class="btn" onclick="filterSelection('overclocking')">Overclocking</button>
-			<button class="btn" onclick="filterSelection('servers')">Servers</button>
-			<button class="btn" onclick="filterSelection('cars')">Cars</button>
-			<button class="btn" onclick="filterSelection('all')">Show all</button>
-		</div>
-
-		<div class="row">
-			<div class="column development">
-				<div class="content">
-					<p>Lorem ipsum</p>
-				</div>
+			<div id="filterContainer">
+				<button class="btn active" onclick="filterSelection('development')">Development</button>
+				<button class="btn" onclick="filterSelection('overclocking')">Overclocking</button>
+				<button class="btn" onclick="filterSelection('servers')">Servers</button>
+				<button class="btn" onclick="filterSelection('cars')">Cars</button>
+				<button class="btn" onclick="filterSelection('all')">Show all</button>
 			</div>
 
-			<div class="column overclocking">
-				<div class="content">
-					<p>Lorem ipsum</p>
+			<div class="row">
+				<div class="column development">
+					<div class="content">
+						<p>Lorem ipsum</p>
+					</div>
 				</div>
-			</div>
 
-			<div class="column servers">
-				<div class="content">
-					<p>Lorem ipsum</p>
+				<div class="column overclocking">
+					<div class="content">
+						<p>Lorem ipsum</p>
+					</div>
 				</div>
-			</div>
 
-			<div class="column cars">
-				<div class="content">
-					<p>Lorem ipsum</p>
+				<div class="column servers">
+					<div class="content">
+						<p>Lorem ipsum</p>
+					</div>
+				</div>
+
+				<div class="column cars">
+					<div class="content">
+						<p>Lorem ipsum</p>
+					</div>
 				</div>
 			</div>
 		</div>
